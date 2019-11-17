c3bc4a96006a7b17f9fdaa06b1410acfe9e21f84
Evgeny Soynov <saboteur@saboteur.me>
2019-11-17 19:09:54 +0100

    Fixes layout name, changes enter position on qwerty layout

--- keymap.c
+++ keymap.c
@@ -6,7 +6,7 @@
 #include "version.h"
 
 enum keyboard_layouts {
-  NORMANN,
+  NORMAN,
   MOUSE,
   QWERTY,
 };
@@ -18,7 +18,7 @@ enum custom_keycodes {
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  /* Keymap 0: Normann layout
+  /* Keymap 0: Norman layout
   *
   * ,--------------------------------------------------.           ,--------------------------------------------------.
   * | Esc    |  1!  |  2@  |  3#  |  4$  |  5%  |  <-  |           |  ->  |   6  |   7  |   8  |   9  |   0  |    -   |
@@ -39,7 +39,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *                                 |      |      | LCmd |       | RCmd |        |      |
   *                                 `--------------------'       `----------------------'
   */
-  [NORMANN] = LAYOUT_ergodox(
+  [NORMAN] = LAYOUT_ergodox(
     // left hand
     KC_ESC,         KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           KC_LEFT,        
     KC_TAB,         KC_Q,           KC_W,           KC_D,           KC_F,           KC_K,           TG(MOUSE),
@@ -112,10 +112,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [QWERTY] = LAYOUT_ergodox(
     // left hand
     KC_EQL,          KC_1,        KC_2,          KC_3,    KC_4,    KC_5,    KC_LEFT,
-    KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(NORMANN),
+    KC_DEL,          KC_Q,        KC_W,          KC_E,    KC_R,    KC_T,    TG(NORMAN),
     KC_BSPC,         KC_A,        KC_S,          KC_D,    KC_F,    KC_G,
     KC_LSFT,         CTL_T(KC_Z), KC_X,          KC_C,    KC_V,    KC_B,    ALL_T(KC_NO),
-    LT(NORMANN,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,
+    LT(NORMAN,KC_GRV), KC_QUOT,     LALT(KC_LSFT), KC_LEFT, KC_RGHT,
 
     //left thumb block
       ALT_T(KC_APP), KC_LGUI,
@@ -125,9 +125,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // right hand
     KC_RGHT,      KC_6,    KC_7,    KC_8,    KC_9,              KC_0,           KC_MINS,
     KC_TRNS,      KC_Y,    KC_U,    KC_I,    KC_O,              KC_P,           KC_BSLS,
-    KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN,           GUI_T(KC_QUOT),
+    KC_H,         KC_J,    KC_K,    KC_L,    KC_SCLN,           KC_ENT,
     MEH_T(KC_NO), KC_N,    KC_M,    KC_COMM, KC_DOT,            CTL_T(KC_SLSH), KC_RSFT,
-    KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, TT(NORMANN),
+    KC_UP,        KC_DOWN, KC_LBRC, KC_RBRC, TT(NORMAN),
 
     //right thumb block
     KC_LALT, CTL_T(KC_ESC),
